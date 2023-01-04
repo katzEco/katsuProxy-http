085a9a8fe00e31f23c7c00df1f62e224924407b5
Suphakit P <me@suphakit.net>
2023-01-05 01:08:48 +0700

    updated: Proxy Server Set :D

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
-//
-// Create your proxy server and set the target in the options.
-//
-httpProxy.createProxyServer({target:'http://localhost:9000'}).listen(8000); // See (†)
+let httpProxy = require('http-proxy')
 
-//
-// Create your target server
-//
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
-  res.end();
-}).listen(1980);
+let link = ""
+
+// Proxy Create :D
+httpProxy.createProxyServer({target: link}).listen(1980);
