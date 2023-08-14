1b34e76f3d87d60674040672570fc56ada6b0b39
Suphakit P <me@suphakit.net>
2023-08-15 03:33:41 +0700

    feat<module>: setup env to connect to proxy server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let httpProxy = require('http-proxy')
+const httpProxy = require('http-proxy')
+require('dotenv').config()
 
-let link = ""
+let link = process.env.PROXY_ENDPOINT
 
 // Proxy Create :D
 httpProxy.createProxyServer({target: link}).listen(1980);
